2a61c82670278626a35ec14998a45e1a77a30822
onensensy <sensyonen@gmail.com>
2022-09-09 08:04:17 +0300

    php conversion

---
+++ report.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>Admin Page</title>
+</head>
+<body>
+  <a href="index.html"> <h1 class="index">Online Outdoor Advertising Space Booking  System</h1></a>
+    <h3 id="User">Booked Spaces Report</h3>
+
+<table>
+    <th>Region</th>
+    <th>City</th>
+    <th>Location</th>
+ 
+
+    <tr>
+        <td>Central</td>
+        <td>Kampala</td>
+        <td>Wandegeya</td>
+    </tr>
+
+    <tr>
+        <td>Eastern</td>
+        <td>Jinja</td>
+        <td>Budondo</td>
+    </tr>
+
+    <tr>
+        <td>Western</td>
+        <td>Mbarara</td>
+        <td>Rutooma</td>
+    </tr>
+</table>
+</body>
+</html>
