2d52e89ce9e97c6a26bfe5a61046a2cfba040da9
onensensy <sensyonen@gmail.com>
2022-09-09 08:35:40 +0300

    Name assignment to form items

--- updateSpace.php
+++ updateSpace.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,20 @@
     <link rel="stylesheet" href="admin.css">
     <title>Update Space</title>
 </head>
-<body>
-    <a href="index.html"><h1>Online Outdoor Advertising Space Booking  System</h1></a>
-    <h3 id="update">Update Space</h3>
 
-    <form>
-        <input type="text" placeholder="Space Id" id="box1"><br>
-        <input type="text" placeholder="Region" id="box2"><br>
-        <input type="text" placeholder="City" id="box4"><br>
-        <input type="text" placeholder="Location" id="box5"><br>
-        <input type="submit" value="Update" id="box3">
+<body>
+    <a href="index.html">
+        <h1>Online Outdoor Advertising Space Booking System</h1>
+    </a>
+    <h3 id="update">Update Space</h3>
+    <form action="major.php" method="post">
+        <input type="text" placeholder="Space Id" name="spaceId" id="box1"><br>
+        <input type="text" placeholder="Region" name="region" id="box2"><br>
+        <input type="text" placeholder="City" name="city" id="box4"><br>
+        <input type="text" placeholder="Location" name="location" id="box5"><br>
+        <input type="submit" value="Save" name="addSpace" id="box3">
         <input type="submit" value="Cancel" id="box6">
     </form>
 </body>
+
 </html>
