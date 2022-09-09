e347c8c83bc62a8a87222f8809f50d15fa4298c3
onensensy <sensyonen@gmail.com>
2022-09-09 09:44:21 +0300

    Feature implementation 1. Borrow 2. Borrow report 3. List of spaces on index page

--- report.php
+++ report.php
@@ -1,5 +1,16 @@
+<?php 
+include 'db.php';
+include 'major.php';
+
+$sql = "SELECT * from report";
+$result=$conn->prepare($sql);
+$result->execute();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,33 +18,30 @@
     <link rel="stylesheet" href="admin.css">
     <title>Admin Page</title>
 </head>
+
 <body>
-  <a href="index.html"> <h1 class="index">Online Outdoor Advertising Space Booking  System</h1></a>
+    <a href="index.html">
+        <h1 class="index">Online Outdoor Advertising Space Booking System</h1>
+    </a>
     <h3 id="User">Booked Spaces Report</h3>
-
-<table>
-    <th>Region</th>
-    <th>City</th>
-    <th>Location</th>
- 
-
-    <tr>
-        <td>Central</td>
-        <td>Kampala</td>
-        <td>Wandegeya</td>
-    </tr>
-
-    <tr>
-        <td>Eastern</td>
-        <td>Jinja</td>
-        <td>Budondo</td>
-    </tr>
-
-    <tr>
-        <td>Western</td>
-        <td>Mbarara</td>
-        <td>Rutooma</td>
-    </tr>
-</table>
+    <table>
+        <th>Region</th>
+        <th>City</th>
+        <th>Location</th>
+        <?php while ($item=$result->fetch(PDO::FETCH_ASSOC)) { ?>
+        <tr>
+            <td>
+                <?php echo $item['region']; ?>
+            </td>
+            <td>
+                <?php echo $item['city']; ?>
+            </td>
+            <td>
+                <?php echo $item['location']; ?>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
 </body>
+
 </html>
