98c7e30277fd6a12009ae3fdcbec9f4879f95a8a
onensensy <sensyonen@gmail.com>
2022-09-09 08:56:57 +0300

    Login/Signup

--- adminSign.php
+++ adminSign.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,16 @@
     <link rel="stylesheet" href="form.css">
     <title>Admin Sign In</title>
 </head>
-<body>
-    <h1>Online Outdoor Advertising Space Booking  System</h1>
-    <h3 id="signIn">Sign In as Admin</h3>
 
-    <form>
-        <input type="text" placeholder="Username" id="box1"><br>
-        <input type="text" placeholder="Password" id="box2"><br>
-        <input type="submit" value="Sign In" id="box3">
-        <a href="adminSign.html">Sign In as Admin</a>
+<body>
+    <h1>Online Outdoor Advertising Space Booking System</h1>
+    <h3 id="signIn">Sign In as Admin</h3>
+    <form action="major.php" method="post">
+        <input type="text" placeholder="Username" name="username" id="box1"><br>
+        <input type="text" placeholder="Password" name="password" id="box2"><br>
+        <input type="submit" value="Sign In" name="adminSign-in" id="box3">
+        <a href="signin.php">Sign in as Normal User</a>
     </form>
 </body>
+
 </html>
